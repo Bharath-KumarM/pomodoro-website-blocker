5124e42adf73ce7a69dee80ee8442ec076a82796
Bharath Kumar <bharathkumar@example.com>
2023-10-07 12:36:03 +0530

    to create and clear notification trigger alarms

--- src/localStorage/localTakeABreakTrackerforRestrict.js
+++ src/localStorage/localTakeABreakTrackerforRestrict.js
@@ -16,18 +16,52 @@ export const setLocalTakeABreakTrackerforRestrict = async (takeABreakTrackerforR
     return true;
 
 }
-export const turnOffLocalTakeABreakTrackerforRestrict = async (isAlarmCleared=false)=>{
+export const turnOffLocalTakeABreakTrackerforRestrict = async ({isForceTurnOff, shouldRefreshSites})=>{
     await setLocalTakeABreakTrackerforRestrict(false)
-    refreshAllRestrictedSites()
+    if (shouldRefreshSites){
+        refreshAllRestrictedSites()
+    }
 
-    if (!isAlarmCleared){
+    if (isForceTurnOff){
         await chrome.alarms.clear('takeABreakForRestrict')
+        await chrome.alarms.clear('takeABreakForRestrictBefore2minute')
+        await chrome.alarms.clear('takeABreakForRestrictBefore1minute')
         console.log('break alarm cleared!!!')
     }
+
     return true;
 }
 export const turnOnLocalTakeABreakTrackerforRestrict = async (takeABreakTrackerforRestrict)=>{
     await setLocalTakeABreakTrackerforRestrict(takeABreakTrackerforRestrict)
     refreshAllRestrictedScreenTabs()
     return true;
+}
+
+export const handleTakeABreakClick = async (timeInMinutes)=>{
+    const currTimeObj = Date.now()
+
+    const minToSecConvertor = 60 // 60 is default value for debugging quick, change inside getTakeABreakTrackerforRestrictData as well
+
+    turnOnLocalTakeABreakTrackerforRestrict(currTimeObj + (timeInMinutes*minToSecConvertor*1000))
+
+    await chrome.alarms.create(
+        `takeABreakForRestrict`,
+        {
+            when: currTimeObj + (timeInMinutes*minToSecConvertor*1000),
+        }
+    )
+    await chrome.alarms.create(
+        `takeABreakForRestrictBefore2minute`,
+        {
+            when: currTimeObj + ((timeInMinutes - 2)*minToSecConvertor*1000),
+        }
+    )
+    await chrome.alarms.create(
+        `takeABreakForRestrictBefore1minute`,
+        {
+            when: currTimeObj + ((timeInMinutes - 1)*minToSecConvertor*1000),
+        }
+    )
+
+
 }
