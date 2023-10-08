fd95ab073c84b46fde494e86dc8d4fc0ec9e88c2
Bharath Kumar <bharathkumar@example.com>
2023-10-08 18:57:49 +0530

    initial setting page setup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,26 +2,35 @@ import { createHelpScreencreenTab, createWelcomeScreencreenTab } from '../../uti
 import style from './Header.module.scss'
 import extensionIcon from '../../../logo_3.png'
 
+import {MdSettings as SettingsIcon} from "react-icons/md"
 
-const Header = (props)=>{
+
+const Header = ({setNavSelect})=>{
 
     return (
         <div className={style.header}>
-            <div className={style.headerCnt}>
-                <img 
-                    className={style.extentionIconImg}
-                    src={extensionIcon} alt="extension icon" 
-                />
-                <h3  
-                    className={style.title}
-                    onClick={()=>{
-                        // createWelcomeScreencreenTab()
-                        createHelpScreencreenTab()
-                    }}
-                >
-                    Be Focused
-                </h3>
+            <div className={style.headerOuterCnt}>
+                <div className={style.headerCnt}>
+                    <img 
+                        className={style.extentionIconImg}
+                        src={extensionIcon} alt="extension icon" 
+                    />
+                    <h3  
+                        className={style.title}
+                        onClick={()=>{
+                            // createWelcomeScreencreenTab()
+                            createHelpScreencreenTab()
+                        }}
+                    >
+                        Be Focused
+                    </h3>
 
+                </div>
+                <div className={style.settingIconCnt}
+                    onClick={()=> setNavSelect('setting')}
+                >
+                    <SettingsIcon />
+                </div>
             </div>
         </div>
     )
